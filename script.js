d488a13ff2cbd54836649173cd34aadbf5cd9ae8
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-07-06 13:51:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+let text =document.getElementById('evaluatedText');
+let counter =document.getElementById('wordCount');
+
+  text.addEventListener('input',()=>{
+     let textlength= text.value;
+	  let count= textlength.replace(" ","").length;
+	  counter.textContent = count;
+  })
